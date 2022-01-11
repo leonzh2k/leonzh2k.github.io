042e4a843936d53771f628c1f72dc81d9a4d363b
leonzh2k <leonzh2k@gmail.com>
2022-01-10 21:07:40 -0800

    added light/dark mode for banner

--- des157b/script.js
+++ des157b/script.js
@@ -4,7 +4,10 @@
     const button = document.querySelector('button');
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
-    const sections = document.querySelectorAll('section')
+    const sections = document.querySelectorAll('section');
+    const clouds = document.querySelectorAll('circle');
+    const hills = document.querySelectorAll('path');
+    const bricks = document.querySelectorAll('rect');
     let mode = 'dark';
 
     button.addEventListener('click', function() {
@@ -15,6 +18,21 @@
             for (const section of sections) {
                 section.className = 'switch';
             }
+            for (const cloud of clouds) {
+                cloud.setAttribute('class', 'switch');
+            }
+            for (const hill of hills) {
+                hill.setAttribute('class', 'switch');
+            }
+            for (const brick of bricks) {
+                if (brick.getAttribute('fill') === '#EA7C7C') {
+                    // console.log("true")
+                    brick.setAttribute('fill', 'rgb(160, 84, 84)');
+                } else {
+                    brick.setAttribute('fill', '#919191');
+                }
+                // brick.setAttribute('class', 'switch');
+            }
             mode = 'light';
         } else {
             body.removeAttribute('class');
@@ -23,7 +41,22 @@
             for (const section of sections) {
                 section.removeAttribute('class');
             }
-            mode = 'dark'
+            for (const cloud of clouds) {
+                cloud.removeAttribute('class');
+            }
+            for (const hill of hills) {
+                hill.removeAttribute('class');
+            }
+            for (const brick of bricks) {
+                if (brick.getAttribute('fill') === 'rgb(160, 84, 84)') {
+                    console.log("true")
+                    brick.setAttribute('fill', '#EA7C7C');
+                } else {
+                    brick.setAttribute('fill', '#CFCFCF');
+                }
+                // brick.setAttribute('class', 'switch');
+            }
+            mode = 'dark';
         }
     })
 })()
