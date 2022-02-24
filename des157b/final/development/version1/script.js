a88cc6cc23e7272bb104a4c9396e8e1ffdae4cb5
leonzh2k <leonzh2k@gmail.com>
2022-02-24 01:17:35 -0800

    added basic page switching

---
+++ des157b/final/development/version1/script.js
@@ -0,0 +1,52 @@
+(() => {
+    console.log("ready");
+    let main = document.querySelector('main');
+    let gallery = document.querySelector('#gallery');
+
+    let viewGallery = document.querySelector("header nav li:first-child");
+    let creatViz = document.querySelector("header nav li:last-child");
+
+    let state = "gallery";
+    // switch to gallery if not already in gallery
+    viewGallery.addEventListener("click", () => {
+        console.log(state);
+        if (state !== "gallery") {
+            let child = main.firstElementChild;
+            main.removeChild(child);
+
+            let vizCount = 16;
+            let gallery = document.createElement("section");
+            gallery.id = "gallery";
+            for (let i = 0; i < vizCount; i++) {
+                let vizThumbNail = document.createElement("div");
+                vizThumbNail.textContent = "viz thumbnail";
+                gallery.appendChild(vizThumbNail);
+            }
+
+            main.append(gallery);
+            state = "gallery";
+        }
+    });
+    // switch to create viz if not already in create viz
+    creatViz.addEventListener("click", () => {
+        console.log(state);
+        if (state !== "create") {
+            console.log("da fuq")
+            let child = main.firstElementChild;
+            main.removeChild(child);
+            let createSection = document.createElement("section");
+            createSection.id = "create";
+            createSection.textContent = "viz graphic"
+            main.appendChild(createSection);
+
+            state = "create";
+        }
+    });
+    // will be fetched from database later
+    let vizCount = 16;
+    for (let i = 0; i < vizCount; i++) {
+        let vizThumbNail = document.createElement("div");
+        vizThumbNail.textContent = "viz thumbnail";
+        gallery.appendChild(vizThumbNail);
+    }
+})();
--- des157b/final/development/version1/scripts.js
+++
@@ -1,5 +0,0 @@
-(() => {
-    console.log("ready");
-    let gallery = document.querySelector('#gallery');
-    console.log(gallery.childElementCount);
-})();
